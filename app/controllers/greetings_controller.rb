170585a25faa8ee8932b9e53aabcd7bcd231c0b8
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2014-10-11 06:00:40 +0800

    Show commit message on branch default.

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,4 +1,15 @@
 class GreetingsController < ApplicationController
   def hello
+  	@commit_message = commit_message
+  end
+
+  private
+
+  def commit_message
+    msg = nil
+    Dir.chdir("../systemfcompiler") do
+      msg = `hg log --branch default --limit 1`
+    end
+    msg
   end
 end
